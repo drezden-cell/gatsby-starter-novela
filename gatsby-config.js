45a04d7a5b98f902dc426bb529014f8c926599e0
drezden-cell <54994314+drezden-cell@users.noreply.github.com>
2019-12-14 19:18:40 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,4 +58,13 @@ module.exports = {
       },
     },
   ],
+    plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-XXXXXXXXX-X",
+      },
+    },
+  ],
 };
